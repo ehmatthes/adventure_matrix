63e38de7aa010a3c4707db331c6c3103564b3aab
Eric Matthes <ehmatthes@gmail.com>
2014-05-23 07:35:31 -0800

    Initial plot works.

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-adventure_matrix
+Adventure Matrix
 ================
 
 A script to generate an adventure matrix for PHS, to guide outdoor programming.
+
+An adventure matrix organizes different outdoor adventures according to their level of physical challenge, and the time required for each adventure. If you want to be able to participate in long, difficult adventures you need to start at the bottom left and work your way up, and to the right.
+
+Developing
+===
+    virtualenv --python=python3.3 venv
+    venv/bin/activate
+    pip install numpy
+    pip install matplotlib
---
+++ matrix_maker.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Make a list of adventures, with physical difficulty
+#  and time required for adventure.
+adventures = [{'name': 'Picnic Rock',
+                   'phys': 7,
+                   'time': 5},
+
+                  {'name': 'Beaver Lake',
+                   'phys': 4,
+                   'time': 3},
+
+                  {'name': 'Totem Park',
+                   'phys': 1,
+                   'time': 2},
+
+                  {'name': 'Alpine Adventure Run',
+                   'phys': 9,
+                   'time': 2},
+
+                  {'name': 'Totem Park',
+                   'phys': 1,
+                   'time': 2},
+
+                  {'name': 'Totem Park',
+                   'phys': 1,
+                   'time': 2},
+
+              ]
+
+y = [dict['phys'] for dict in adventures]
+x = [dict['time'] for dict in adventures]
+labels = [dict['name'] for dict in adventures]
+
+fig, ax = plt.subplots()
+# Can just place labels, rather than plotting invisilbe points.
+#  See: http://stackoverflow.com/questions/14827650/pyplot-scatter-plot-marker-size
+ax.scatter(x,y, s=0)
+
+for i, txt in enumerate(labels):
+    ax.annotate(txt, (x[i], y[i]))
+
+
+plt.show()
